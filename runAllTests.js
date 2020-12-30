339a6da07fafface5265108d7cd2cb9a7bba2d00
Lincher6 <dober-01@yandex.ua>
2020-12-30 12:10:40 +0200

    semicolon refactoring

--- runAllTests.js
+++ runAllTests.js
@@ -1,5 +1,5 @@
-require("./tests/test_1")
-require("./tests/test_2")
-require("./tests/test_3")
-require("./tests/test_4")
-require("./tests/test_5")
+require("./tests/test_1");
+require("./tests/test_2");
+require("./tests/test_3");
+require("./tests/test_4");
+require("./tests/test_5");
--- tests/test_4.js
+++ tests/test_4.js
@@ -3,6 +3,6 @@ const resolve = require("../tasks/task_4.js");
 
 arr1 = [2,3,1,3,2,4,6,7,9,2,19];
 arr2 = [2,1,4,3,9,6];
-result = [2,2,2,1,4,3,3,9,6,7,19]
+result = [2,2,2,1,4,3,3,9,6,7,19];
 
 assert.notStrictEqual(resolve(arr1, arr2), result);
